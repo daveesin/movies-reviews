8d70406e0ae69e24c3358066b864475872d2497c
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-18 11:11:14 -0300

    Create a initial structure to the navbar to test

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -1,6 +1,20 @@
 function Navbar() {
     return(
-        <h1>Navbar</h1>
+        <div className="flex justify-between items-center gap-3 p-6 w-full h-full max-h-60 shadow bg-movies-card text-movies-text">
+
+            <div>
+                <span>Logo</span>
+            </div>
+
+            <div>
+                <span>List</span>
+            </div>
+
+            <div>
+                <span>Login/Settings (?)</span>
+            </div>
+
+        </div>
     );
 }
 
