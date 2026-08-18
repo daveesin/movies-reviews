04363fc09f9816a99880ddb2a55254d4761cd9ef
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-18 13:33:33 -0300

    An example button to see how is gonna be the buttons in the storage menu

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Clapperboard, DatabaseBackup } from 'lucide-react';
+import { Clapperboard, DatabaseBackup, Trash } from 'lucide-react';
 
 function Navbar() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return(
         <div className='flex justify-between items-center gap-3 px-10 py-8 shadow bg-movies-card text-movies-text'>
 
@@ -23,9 +27,20 @@ function Navbar() {
             </nav>
 
             <div className='relative'>
-                <button className='hover:cursor-pointer'>
+                <button 
+                className='hover:cursor-pointer'
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                >
                     <DatabaseBackup className='w-8 h-8 text-movies-accent transition-colors hover:text-movies-accent-dark' />
                 </button>
+                {isMenuOpen && (
+                    <div className='absolute right-0 mt-3 w-52 bg-movies-card border border-movies-border rounded-xl shadow-2xl z-50 p-2 flex flex-col gap-1'>
+                        <button className='flex gap-1 items-center px-2 py-3 rounded-md font-semibold text-movies-accent transition-colors hover:cursor-pointer hover:bg-movies-danger hover:text-movies-text'>
+                            <Trash />
+                            <span>Clean All Data</span>
+                        </button>
+                    </div>
+                )}
             </div>
 
         </div>
