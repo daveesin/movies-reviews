fb12bd8047c1b5cd57c549a3cb83575a75da0d38
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-19 16:53:01 -0300

    Create a service to use the seach movie endpoint

--- src/services/moviesApi.js
+++ src/services/moviesApi.js
@@ -12,10 +12,25 @@ const options = {
 export async function getMovies(page=1) {
 
     const response = await fetch(`${BASE_URL}/discover/movie?page=${page}`, options);
-    if(!response.ok){
+    if(!response.ok) {
         throw new Error('Fail loading movies from the TMDB API.')
     }
     
+    const data = await response.json();
+    return {
+        movies: data.results,
+        currentPage: data.page,
+        totalPages: data.total_pages,
+    }
+}
+
+export async function searchMovies(searchTerm, page=1) {
+    
+    const response = await fetch(`${BASE_URL}/search/movie?query=${encodeURIComponent(searchTerm)}&page=${page}`);
+    if(!response.ok) {
+        throw new Error('Fail loading seached movies from the MDB API.')
+    }
+
     const data = await response.json();
     return {
         movies: data.results,
