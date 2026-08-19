235c9d8dd4c5a86e440e7f153b7db26c7c9e5af2
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-19 14:34:16 -0300

    Adapt my aplication to render the new component in the path '/media'

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Home from './pages/Home.jsx'
+import Catalog from './pages/Catalog.jsx'
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />
+      },
+      {
+        path: '/media',
+        element: <Catalog />
       }
     ]
   },
