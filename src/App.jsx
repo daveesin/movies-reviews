dfb6345d415767762abfe3de162f4bc3f1435f70
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-19 14:33:52 -0300

    Adapt my App.jsx to load this data using the JS service created and send to the other components with Outlet context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './common/Navbar';
 import Footer from './common/Footer';
 
+import { getMovies } from './services/moviesApi';
+
 function App() {
+
+  const [catalog, setCatalog] = useState([]);
+
+  useEffect(() => {
+    async function fetchInitialMovies() {
+
+      const moviesList = await getMovies();
+      setCatalog(moviesList);
+    }
+
+    fetchInitialMovies();
+
+  }, []);
+
   return(
     <div className='w-full h-full flex flex-col '>
 
       <Navbar />
-      <Outlet />
+      <Outlet context={{ catalog }} />
       <Footer />
     
     </div>
