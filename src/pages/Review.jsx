1aeb79c8b0bfb648e08cb52fd1d3ce8a096aa465
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-21 10:55:32 -0300

    Start to structure the states of the Review creation component and create the useEffect to load movie data

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,4 +1,35 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieDetails } from '../services/moviesApi';
+
 function Review() {
+
+    const { movieId } = useParams();
+    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [loading, setLoading] = useState(true);
+    
+    const [comment, setComment] = useState('');
+    const [overallRating, setOverallRating] = useState(0.0);
+    const [ratings, setRatings] = useState(null);
+    const [review, setReview] = useState(null);
+
+    useEffect(() => {
+        async function fetchMovieDetails() {
+            setLoading(true);
+
+            try{
+                const movieDetails = await getMovieDetails(movieId);
+                setSelectedMovie(movieDetails);
+            } catch(err) {
+                console.log(err.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchMovieDetails();
+    }, [movieId])
+
     return(
         <h1>Review</h1>
     );
